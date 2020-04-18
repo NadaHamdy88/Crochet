e2b5e352988d5bdf13b4944a9430ea9fa65f79da
nadahamdy88 <nadahamdy88@gmail.com>
2020-04-18 06:42:58 +0200

    crochet

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+        // Show Navbar when Click on icon
+    $('i.icon').click(function(){
+        $('.nav-list').slideToggle()
+    });    
+
+
+     // Show selling when click on button
+     $('.home-btn').click(function(){
+        $(this).show(function(){
+            $('.selling').show()
+        })
+       
+    }); 
+
+    
+     //products Buttons change bg
+    $('.products-menu button').click(function(){
+        $(this).addClass('focus_button').siblings().removeClass('focus_button')
+    });
+    $('.images').mixItUp();
+
+      //popup video in about-video section
+      $('.play-btn').magnificPopup({
+        type: 'iframe',
+        // delegate: 'a'
+    }); 
+
+    $('.img-poup').magnificPopup({
+        type: 'image',
+        gallery:{
+        enabled:true,
+        }
+    });
+  
+     //Show bag-steps when click on button
+     $('.more').click(function(){
+        $(this).show(function(){
+            $('.bag-steps').show()
+        })
+   });
+
+   //
+   var target = location.href.split("=")[1];
+   $('#'+ target).trigger("click");
+   
+});
